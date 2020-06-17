820daa641e891ae7a09686b5afa7c20f11b88f1d
ElenaKusevska <elena.kusevska@gmail.com>
2020-06-17 21:22:48 +0200

    SQLite

---
+++ SQL/Chicago_hardship_data_sqlite3.py
@@ -0,0 +1,29 @@
+# Based on:
+# https://www.coursera.org/learn/sql-data-science/ungradedLti/uCNuW/hands-on-lab-3-analyzing-a-real-world-data-set
+# https://www.dataquest.io/blog/python-pandas-databases/
+# https://stackoverflow.com/questions/14431646/how-to-write-pandas-dataframe-to-sqlite-with-index
+
+import pandas
+import sqlite3
+
+#Pull CSD (Chicago Socioeconomic Data) into Pandas dataframe
+CSD = pandas.read_csv('https://data.cityofchicago.org/resource/jcxq-k9xf.csv')
+print(CSD.iloc[:5])
+
+# Establish connection to local database
+conn = sqlite3.connect('./CSD.sqlite') # Create database
+cur = conn.cursor()
+
+# Drop table
+cur.execute('''DROP TABLE IF EXISTS CSD''')
+results = cur.fetchall()
+print(results)
+
+# Write the Pandas dataframe to sqlite3 database
+CSD.to_sql('CSD', conn, if_exists='replace', index=False)
+cur.execute('select * from CSD limit 5;')
+results = cur.fetchall()
+print(results)
+
+conn.close()
+
