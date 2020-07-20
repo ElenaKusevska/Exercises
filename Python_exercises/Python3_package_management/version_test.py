4324d5ce15b15f3577cfba202a476f623d890d07
ElenaKusevska <elena.kusevska@gmail.com>
2020-07-20 18:42:53 +0200

    rms

--- Python_exercises/Python3_package_management/version_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.6
-import numpy
-import inspect
-
-print("'inspect.getfile(numpy)' gives: ", inspect.getfile(numpy))
-print(" ")
-print("'numpy.__version__' gives: ", numpy.__version__)
