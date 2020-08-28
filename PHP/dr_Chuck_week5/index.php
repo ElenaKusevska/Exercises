2f927457d1136a2960ca40c99606d83011e88d61
Elena Kusevska <31021042+ElenaKusevska@users.noreply.github.com>
2020-08-28 06:38:17 +0200

    PHP

---
+++ PHP/dr_Chuck_week5/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Elena Kusevska PHP</title>
+<meta charset="UTF-8">
+</head>
+    
+<body>
+
+<h1> Elena Kusevska PHP </h1>
+
+<pre>ASCII Art
+    **********
+    **********
+    **
+    **
+    ******
+    ******
+    **
+    **
+    **********
+    **********
+</pre>
+
+<p>
+<?php
+echo('The SHA256 hash of "Elena Kusevska" is ');
+print hash('sha256', 'Charles Severance');
+?>
+</p>
+
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+
+</html>
