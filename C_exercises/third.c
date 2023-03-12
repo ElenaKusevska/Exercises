e8460f6099df4c465c9188304cdf083fa96509bc
ElenaKusevska <elena.kusevska@gmail.com>
2023-03-12 20:36:27 +0100

    edit to comments

--- C_exercises/third.c
+++ C_exercises/third.c
@@ -5,19 +5,13 @@
 // reading from stdin:
 //-------------------------
 
-//-----------------
-// Using scanf:
-//-----------------
-
 int main()
 {
 
 char str [20];
 int i, c;
 
-//-----------------
 // using scanf:
-//------------------
 
 printf("What's your name?");
 scanf("%10s", str);
@@ -25,9 +19,7 @@ printf("how old are you?");
 scanf("%d", &i);
 printf("%10s, %d\n", str, i);
 
-//--------------------
 // using getchar:
-// -------------------
 
 printf("give me a number");
 
